887445e119754647e3f93bc81a3ba982e892a649
ManuelCandiani <manuel.candiani@mail.polimi.it>
2017-05-09 14:39:20 +0200

    GUI - Ridimensionamento interfaccia

--- modules/client/src/main/java/it/polimi/ingsw/lim/client/gui/ControllerConnection.java
+++ modules/client/src/main/java/it/polimi/ingsw/lim/client/gui/ControllerConnection.java
@@ -8,6 +8,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -75,5 +76,6 @@ public class ControllerConnection implements Initializable
 	@Override
 	public void initialize(URL fxmlFileLocation, ResourceBundle resourceBundle)
 	{
+		this.connectionButton.prefWidthProperty().bind(((VBox) this.connectionButton.getParent()).widthProperty());
 	}
 }
