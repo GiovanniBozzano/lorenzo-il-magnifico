69c895efa06ef8ad484e38533cef1c8536e18bfc
Giovanni Bozzano <giovanni.bozzano.1996@gmail.com>
2017-05-12 18:45:09 +0200

    Generale: - Metodi per criptare e decriptare le stringhe sensibili.

--- modules/common/src/main/java/it/polimi/ingsw/lim/common/utils/CommonUtils.java
+++ modules/common/src/main/java/it/polimi/ingsw/lim/common/utils/CommonUtils.java
@@ -7,12 +7,24 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
 import java.io.IOException;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
 import java.util.logging.Level;
 
 public class CommonUtils
 {
 	public static final String VERSION = "1.0";
+	private static final byte[] IV = { 120, 7, 71, 53, 82, 112, 20, 8, 1, 19, 68, 26, 35, 56, 118, 23 };
+	private static final byte[] CRYPT_KEY = { 55, 78, 54, 102, 106, 89, 70, 69, 112, 83, 71, 88, 80, 98, 55, 108 };
 
 	private CommonUtils()
 	{
@@ -103,5 +115,39 @@ public class CommonUtils
 			}
 		}
 		return true;
+	}
+
+	/**
+	 * Encrypt a string with a 128 bit key.
+	 * @param text the string to be encrypted.
+	 * @return the encrypted string.
+	 */
+	public static String encrypt(String text)
+	{
+		try {
+			Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING");
+			cipher.init(Cipher.ENCRYPT_MODE, new SecretKeySpec(CommonUtils.CRYPT_KEY, "AES"), new IvParameterSpec(CommonUtils.IV));
+			return Base64.getEncoder().encodeToString(cipher.doFinal(text.getBytes()));
+		} catch (NoSuchAlgorithmException | NoSuchPaddingException | InvalidKeyException | InvalidAlgorithmParameterException | IllegalBlockSizeException | BadPaddingException exception) {
+			Instance.getLogger().log(Level.SEVERE, LogFormatter.EXCEPTION_MESSAGE, exception);
+		}
+		return null;
+	}
+
+	/**
+	 * Decrypt a string with a 128 bit key.
+	 * @param text the string to be decrypted.
+	 * @return the decrypted string.
+	 */
+	public static String decrypt(String text)
+	{
+		try {
+			Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING");
+			cipher.init(Cipher.DECRYPT_MODE, new SecretKeySpec(CommonUtils.CRYPT_KEY, "AES"), new IvParameterSpec(CommonUtils.IV));
+			return new String(cipher.doFinal(Base64.getDecoder().decode(text)));
+		} catch (NoSuchAlgorithmException | NoSuchPaddingException | InvalidKeyException | InvalidAlgorithmParameterException | IllegalBlockSizeException | BadPaddingException exception) {
+			Instance.getLogger().log(Level.SEVERE, LogFormatter.EXCEPTION_MESSAGE, exception);
+		}
+		return null;
 	}
 }
