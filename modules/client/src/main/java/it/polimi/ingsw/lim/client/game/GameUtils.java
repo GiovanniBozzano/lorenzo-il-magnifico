3cc0ce84cba8976ceafd5c2d36d9b52ed5fa5b85
GiovanniBozzano <giovanni.bozzano.1996@gmail.com>
2017-06-13 21:50:41 +0200

    Gioco - Struttura dei pacchetti di gioco

--- modules/client/src/main/java/it/polimi/ingsw/lim/client/game/GameUtils.java
+++ modules/client/src/main/java/it/polimi/ingsw/lim/client/game/GameUtils.java
@@ -1,10 +1,9 @@
 package it.polimi.ingsw.lim.client.game;
 
-import it.polimi.ingsw.lim.common.game.CouncilPalaceRewardInformations;
-import it.polimi.ingsw.lim.common.game.DevelopmentCardInformations;
+import it.polimi.ingsw.lim.common.enums.Period;
+import it.polimi.ingsw.lim.common.game.*;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class GameUtils
 {
@@ -12,6 +11,10 @@ public class GameUtils
 	private static final List<DevelopmentCardInformations> DEVELOPMENT_CARDS_CHARACTER = new ArrayList<>();
 	private static final List<DevelopmentCardInformations> DEVELOPMENT_CARDS_TERRITORY = new ArrayList<>();
 	private static final List<DevelopmentCardInformations> DEVELOPMENT_CARDS_VENTURE = new ArrayList<>();
+	private static final List<LeaderCardInformations> LEADER_CARDS_INFORMATIONS = new ArrayList<>();
+	private static final List<ExcommunicationTileInformations> EXCOMMUNICATION_TILES_INFORMATIONS = new ArrayList<>();
+	private static final Map<Period, Integer> EXCOMMUNICATION_TILES = new EnumMap<>(Period.class);
+	private static final Map<Integer, PlayerData> PLAYERS_DATA = new HashMap<>();
 	private static final List<CouncilPalaceRewardInformations> COUNCIL_PALACE_REWARDS = new ArrayList<>();
 
 	private GameUtils()
@@ -41,5 +44,25 @@ public class GameUtils
 	public static List<CouncilPalaceRewardInformations> getCouncilPalaceRewards()
 	{
 		return GameUtils.COUNCIL_PALACE_REWARDS;
+	}
+
+	public static List<LeaderCardInformations> getLeaderCardsInformations()
+	{
+		return GameUtils.LEADER_CARDS_INFORMATIONS;
+	}
+
+	public static List<ExcommunicationTileInformations> getExcommunicationTilesInformations()
+	{
+		return GameUtils.EXCOMMUNICATION_TILES_INFORMATIONS;
+	}
+
+	public static Map<Period, Integer> getExcommunicationTiles()
+	{
+		return GameUtils.EXCOMMUNICATION_TILES;
+	}
+
+	public static Map<Integer, PlayerData> getPlayersData()
+	{
+		return GameUtils.PLAYERS_DATA;
 	}
 }
