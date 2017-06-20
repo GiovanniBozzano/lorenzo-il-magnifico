273e446762bfc87e45ebe63f92fdaf3f80c805a8
ManuelCandiani <manuel.candiani@mail.polimi.it>
2017-06-20 19:09:55 +0200

    Generale -Aggiornate azioni relative alle LeaderCards

---
+++ modules/common/src/main/java/it/polimi/ingsw/lim/common/game/actions/ActionInformationsRewardLorenzoDeMediciLeader.java
@@ -0,0 +1,11 @@
+package it.polimi.ingsw.lim.common.game.actions;
+
+import it.polimi.ingsw.lim.common.enums.ActionType;
+
+public class ActionInformationsRewardLorenzoDeMediciLeader extends ActionInformations
+{
+	public ActionInformationsRewardLorenzoDeMediciLeader()
+	{
+		super(ActionType.REWARD_LORENZO_DE_MEDICI_LEADER);
+	}
+}
--- modules/server/src/main/java/it/polimi/ingsw/lim/server/game/actions/ActionChooseLorenzoDeMediciLeader.java
+++ modules/server/src/main/java/it/polimi/ingsw/lim/server/game/actions/ActionChooseLorenzoDeMediciLeader.java
@@ -1,7 +1,18 @@
 package it.polimi.ingsw.lim.server.game.actions;
 
 import it.polimi.ingsw.lim.common.game.actions.ActionInformationsChooseLorenzoDeMediciLeader;
+import it.polimi.ingsw.lim.server.game.GameHandler;
+import it.polimi.ingsw.lim.server.game.Room;
+import it.polimi.ingsw.lim.server.game.cards.CardsHandler;
+import it.polimi.ingsw.lim.server.game.cards.LeaderCard;
+import it.polimi.ingsw.lim.server.game.cards.leaders.LeaderCardReward;
 import it.polimi.ingsw.lim.server.game.player.Player;
+import it.polimi.ingsw.lim.server.game.utils.Phase;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class ActionChooseLorenzoDeMediciLeader extends ActionInformationsChooseLorenzoDeMediciLeader implements IAction
 {
@@ -16,11 +27,50 @@ public class ActionChooseLorenzoDeMediciLeader extends ActionInformationsChooseL
 	@Override
 	public boolean isLegal()
 	{
-		return false;
+		// check if the player is inside a room
+		Room room = Room.getPlayerRoom(this.player.getConnection());
+		if (room == null) {
+			return false;
+		}
+		// check if the game has started
+		GameHandler gameHandler = room.getGameHandler();
+		if (gameHandler == null) {
+			return false;
+		}
+		// check if it is the player's turn
+		if (this.player != gameHandler.getTurnPlayer()) {
+			return false;
+		}
+		// check whether the server expects the player to make this action
+		if (gameHandler.getExpectedAction() != null) {
+			return false;
+		}
+		// check if the chosen leader card is valid
+		List<Integer> availableLeaderCards  = new ArrayList<>();
+		for (Player otherPlayer : gameHandler.getTurnOrder()) {
+			if (otherPlayer != this.player) {
+				for (LeaderCard leaderCard : otherPlayer.getPlayerCardHandler().getLeaderCards()) {
+					if (leaderCard.isPlayed()) {
+						availableLeaderCards.add(leaderCard.getIndex());
+					}
+				}
+			}
+		}
+		return availableLeaderCards.contains(this.getLeaderCardIndex());
 	}
 
 	@Override
 	public void apply()
 	{
+		Room room = Room.getPlayerRoom(this.player.getConnection());
+		if (room == null) {
+			return;
+		}
+		GameHandler gameHandler = room.getGameHandler();
+		if (gameHandler == null) {
+			return;
+		}
+		gameHandler.setCurrentPhase(Phase.LEADER);
+
 	}
 }
