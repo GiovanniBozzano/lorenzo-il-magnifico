26d5840fcbe263a1eaa4cf0f2422ebe145c2e04c
ManuelCandiani <manuel.candiani@mail.polimi.it>
2017-07-03 00:32:06 +0200

    CLI -ExcommunicationChoice

--- modules/client/src/main/java/it/polimi/ingsw/lim/client/cli/CLIHandlerExcommunicationChoice.java
+++ modules/client/src/main/java/it/polimi/ingsw/lim/client/cli/CLIHandlerExcommunicationChoice.java
@@ -2,7 +2,6 @@ package it.polimi.ingsw.lim.client.cli;
 
 import it.polimi.ingsw.lim.client.Client;
 import it.polimi.ingsw.lim.common.cli.ICLIHandler;
-import it.polimi.ingsw.lim.common.cli.IInputHandler;
 import it.polimi.ingsw.lim.common.utils.CommonUtils;
 
 import java.util.HashMap;
@@ -11,13 +10,11 @@ import java.util.logging.Level;
 
 public class CLIHandlerExcommunicationChoice implements ICLIHandler
 {
-	private static final Map<Integer, IInputHandler> EXCOMMUNICATION_CHOICES = new HashMap<>();
+	private static final Map<Integer, Boolean> EXCOMMUNICATION_CHOICES = new HashMap<>();
 
 	static {
-		CLIHandlerExcommunicationChoice.EXCOMMUNICATION_CHOICES.put(1, cliHandler -> {
-		});
-		CLIHandlerExcommunicationChoice.EXCOMMUNICATION_CHOICES.put(2, cliHandler -> {
-		});
+		CLIHandlerExcommunicationChoice.EXCOMMUNICATION_CHOICES.put(1, Boolean.TRUE);
+		CLIHandlerExcommunicationChoice.EXCOMMUNICATION_CHOICES.put(2, Boolean.FALSE);
 	}
 
 	@Override
@@ -35,13 +32,13 @@ public class CLIHandlerExcommunicationChoice implements ICLIHandler
 	private void askExcommunicationChoice()
 	{
 		Client.getLogger().log(Level.INFO, "Enter your choice...");
-		Client.getLogger().log(Level.INFO, "1 - ...");
-		Client.getLogger().log(Level.INFO, "2 - ...");
+		Client.getLogger().log(Level.INFO, "1 - Do not praise the SUN...");
+		Client.getLogger().log(Level.INFO, "2 - Praise the SUN...");
 		String input;
 		do {
 			input = Client.getInstance().getCliScanner().nextLine();
 		}
 		while (!CommonUtils.isInteger(input) || !CLIHandlerExcommunicationChoice.EXCOMMUNICATION_CHOICES.containsKey(Integer.parseInt(input)));
-		CLIHandlerExcommunicationChoice.EXCOMMUNICATION_CHOICES.get(Integer.parseInt(input)).execute(this);
+		Client.getInstance().getConnectionHandler().sendGameExcommunicationPlayerChoice(CLIHandlerExcommunicationChoice.EXCOMMUNICATION_CHOICES.get(Integer.parseInt(input)));
 	}
 }
